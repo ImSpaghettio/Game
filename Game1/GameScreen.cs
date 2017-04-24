46d486f8a1ec50c75aa0daa5c98688ef5687be28
ImSpaghettio <aburroughs00@live.com>
2017-04-24 18:50:27 -0400

    Screen Class

---
+++ Game1/GameScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class GameScreen : Screen
+    {
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+    }
+}
--- Game1/ScreenManager.cs
+++ Game1/ScreenManager.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,57 @@ namespace Game1
 {
     public class ScreenManager
     {
+        private static ScreenManager instance;
+        private static Screen currentScreen;
+        public ContentManager content { get; set; }
+
+        public Screen CurrentScreen
+        {
+            get
+            {
+                return currentScreen;
+            }
+
+            set
+            {
+                currentScreen = value;
+            }
+        }
+
+        public static ScreenManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new ScreenManager();
+
+                    return instance;
+            }
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public void LoadContent()
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
     }
 }
